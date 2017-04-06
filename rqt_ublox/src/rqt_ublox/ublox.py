0e727141812124586707dbf4f0307ef494898774
Marek Materzok <tilk@tilk.eu>
2017-04-06 21:27:42 +0200

    Changed RQT widgets to labels.

--- rqt_ublox/src/rqt_ublox/ublox.py
+++ rqt_ublox/src/rqt_ublox/ublox.py
@@ -9,6 +9,7 @@ from python_qt_binding.QtWidgets import QWidget
 from ublox_msgs.msg import NavPVT
 
 class UBloxStatus(Plugin):
+    fixTypes = {0: "no fix", 1: "DR", 2: "2D", 3: "3D", 4: "DR+GNSS", 5: "TIME"}
 
     def __init__(self, context):
         super(UBloxStatus, self).__init__(context)
@@ -49,19 +50,20 @@ class UBloxStatus(Plugin):
         pass
 
     def pvt_callback(self, m):
-        print "Callback! %d" % m.iTOW
-        self._widget.lon.display(m.lon / 1e7)
-        self._widget.lat.display(m.lat / 1e7)
-        self._widget.alt.display(m.height / 1e3)
-        self._widget.altMSL.display(m.hMSL / 1e3)
-        self._widget.hAcc.display(m.hAcc / 1e4)
-        self._widget.vAcc.display(m.vAcc / 1e4)
-        self._widget.pDOP.display(m.pDOP / 1e2)
-        self._widget.sat.display(m.numSV)
-        self._widget.spd.display(m.gSpeed / 1e3)
-        self._widget.spdAcc.display(m.sAcc / 1e3)
-        self._widget.head.display(m.headMot / 1e5)
-        self._widget.headAcc.display(m.headAcc / 1e5)
+        self._widget.fixMode.setText(self.fixTypes[m.fixType])
+        self._widget.utcTime.setText("%0.2d:%0.2d:%0.2d" % (m.hour, m.min, m.sec))
+        self._widget.lon.setText("%.4f" % (m.lon / 1e7))
+        self._widget.lat.setText("%.4f" % (m.lat / 1e7))
+        self._widget.alt.setText("%.2f m" % (m.height / 1e3))
+        self._widget.altMSL.setText("%.2f m" % (m.hMSL / 1e3))
+        self._widget.hAcc.setText("%.2f m" % (m.hAcc / 1e4))
+        self._widget.vAcc.setText("%.2f m" % (m.vAcc / 1e4))
+        self._widget.pDOP.setText("%.2f" % (m.pDOP / 1e2))
+        self._widget.sat.setText("%d" % m.numSV)
+        self._widget.spd.setText("%.2f m/s" % (m.gSpeed / 1e3))
+        self._widget.spdAcc.setText("%.2f m/s" % (m.sAcc / 1e3))
+        self._widget.head.setText("%.2f" % (m.headMot / 1e5))
+        self._widget.headAcc.setText("%.2f" % (m.headAcc / 1e5))
 
     #def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure
